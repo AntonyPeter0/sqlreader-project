4feb3f9dd5e286eb7b27c01a5513fd77eb0fc2c7
AntonyPeter0 <antonypeter1998@gmail.com>
2023-07-29 22:55:20 +0530

    added storage aaccount module

---
+++ modules/storageaccount/variable.tf
@@ -0,0 +1,40 @@
+variable "storage_account_name" {
+  type = string
+  description = "Name of storage account"
+}
+
+variable "resource_group_name" {
+    type = string
+    description = "This is the RG name"
+}
+
+variable "location_name" {
+    type = string
+    description = "This is the location t"
+  
+}
+
+variable "container_name" {
+    type = string
+    description = "This is the container name"
+  
+}
+
+variable "app_container_name" {
+    type = string
+    description = "This is the container name for the application"
+  
+}
+
+variable "container_access_type" {
+    type = string
+    description = "This is the container access type"
+    default = "blob"
+  
+}
+
+variable "storage_blobs" {
+    type = map
+    description = "This is the name of  storage blob "
+  
+}
